fbddabee6b300d625528903cb19132444dc0eddc
Maryja Peredryj <chysapko@gmail.com>
2018-06-13 23:16:14 +0300

    Deploy disabled, readme edited

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # London Transport Application
 
-Link:
-
 It can:
 - search stops;
 - show lines list for buses, tube and river buses;
 - show stop list for lines;
 - show arrival info for stop points.
 
-Author Maria Peredriy
+To start run `npm run start`
+
+_Author Maria Peredriy_
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/dist/')));
-app.use('/src/assets', express.static(path.join(__dirname, '/src/assets/')));
+app.use(express.static(path.join(__dirname, '/')));
 
 app.listen(process.env.PORT || 8080);
